0f757f9291aea1428a47fdc0f0203adb7da08077
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-09-05 14:14:52 +0530

    fix jwt token

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/users')
 const Order = require('../models/orders')
 const { privateKey } = require('../../secrets/jwtPrivateKey')
-const { oauth2Client, oauth2, url } = require('../../oAuth/oAuthGoogle')
+const { oauth2Client, oauth2, userLoginURL } = require('../../oAuth/oAuthGoogle')
 
 function serveAppropriatePage (req, res, next, jwToken, statusCode) {
   if (req.isSignedIn) {
@@ -13,7 +13,7 @@ function serveAppropriatePage (req, res, next, jwToken, statusCode) {
     res.status(200).json({ message: 'login sucessful' })
   } else {
     //  res.redirect('http://localhost:8000/login')
-    res.status(200).json({ link: url })
+    res.status(200).json({ link: userLoginURL })
   }
 }
 
@@ -35,7 +35,7 @@ async function placeOrder (req, res) {
 
 async function handleUserInfo (req, res) {
   try {
-    let tokenObj = await getAccessToken(req)
+    let tokenObj = await getAccessToken(req.query.code)
     if (tokenObj) {
       oauth2Client.setCredentials(tokenObj.tokens)
       let userInfo = await getUserInfo()
@@ -51,9 +51,9 @@ async function handleUserInfo (req, res) {
   }
 }
 
-async function getAccessToken (req) {
+async function getAccessToken (code) {
   try {
-    let tokenObj = await oauth2Client.getToken(req.query.code)
+    let tokenObj = await oauth2Client.getToken(code)
     return tokenObj
   } catch (error) {
     return null
@@ -76,11 +76,12 @@ async function handleUserRecord (userinfo, token) {
   try {
     let dbSearchResult = await User.findOne({ emailID: userinfo.email }).exec()
     if (!dbSearchResult.jwt.includes(token)) {
+      dbSearchResult.jwt.push(token)
       let user = new User({
         name: userinfo.name,
         emailID: userinfo.email,
         profilePicture: userinfo.picture,
-        jwt: dbSearchResult.jwt.push(token)
+        jwt: dbSearchResult.jwt
       })
       return (await user.save())
     }
@@ -95,11 +96,11 @@ async function signoutUser (req, res) {
     let deletion = await deleteJWTValue(req.emailID)
     if (deletion) {
       res.clearCookie('access_token', { path: '/' })
-      res.status(200).json({ message: 'you have been logged out successfully, to login please click on the link', link: url })
+      res.status(200).json({ message: 'you have been logged out successfully, to login please click on the link', link: userLoginURL })
     }
     res.status(500).json({ message: 'logged out operation was unsuccessfull' })
   } else {
-    res.status(401).json({ message: 'you are not logged in and you can login by clicking on the link', link: url })
+    res.status(401).json({ message: 'you are not logged in and you can login by clicking on the link', link: userLoginURL })
   }
 }
 
@@ -113,7 +114,7 @@ async function deleteJWTValue (emailID) {
 }
 
 function getLoginURLAndSend (req, res) {
-  res.status(200).json({url: url})
+  res.status(200).json({url: userLoginURL})
 }
 
 async function getOrdersAndSend (req, res) {
