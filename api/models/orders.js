87b65e4996c80140b33b0aea3e2fce4139431c48
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-09-06 17:28:16 +0530

    add to and from to orders

--- api/models/orders.js
+++ api/models/orders.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 
 const orderSchema = new mongoose.Schema({
   description: { type: String, required: true },
+  from: { type: String, required: true },
+  to: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   runner: { type: mongoose.Schema.Types.ObjectId, ref: 'Runner' },
   status: { type: String, required: true, default: 'placed' },
