05086e3abffaaedcc3d80f629c996ebb28ec7f08
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-09-06 16:59:03 +0530

    remove root (domain/ ) hadler

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const path = require('path')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-const { authenticate } = require('./middlewares/authenticate')
-const { serveAppropriatePage } = require('./api/controllers/users')
 
 const usersRoute = require('./api/routes/users')
 const runnersRoute = require('./api/routes/runners')
@@ -16,10 +13,6 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(cookieParser())
-
-const homePageHandler = (req, res, next) => serveAppropriatePage(req, res, next)
-
-app.get('/', authenticate, homePageHandler)
 
 app.use('/user', usersRoute)
 app.use('/runner', runnersRoute)
