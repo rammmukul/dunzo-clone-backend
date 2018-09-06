48259f61f5fb507618030b1b050594ba723819bc
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-09-06 17:31:20 +0530

    remove body-parser

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,16 +1,15 @@
 const router = require('express').Router()
-const urlEncodedParser = require('body-parser').urlencoded({ extended: false })
 const usersController = require('../controllers/users')
 const { authenticate } = require('../../middlewares/authenticate')
 
 // doesn't need authentication middleware
-router.get('/oauthcallback', usersController.handleUserInfo)
+router.get('/oauthcallback', usersController.oauthcallback)
 router.get('/getLoginURL', usersController.getLoginURLAndSend)
 
 // Needs authentication middleware
 router.get('/getOrders', authenticate, usersController.getOrdersAndSend)
 router.get('/getOrderDetails', authenticate, usersController.getOrderDetailsAndSend)
 router.get('/signout', authenticate, usersController.signoutUser)
-router.post('/placeorder', urlEncodedParser, authenticate, usersController.placeOrder)
+router.post('/placeorder', authenticate, usersController.placeOrder)
 
 module.exports = router
