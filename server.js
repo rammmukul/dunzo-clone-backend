c41e6a8f5d80122f1adb21c2bcef756b14693383
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-09-06 20:02:36 +0530

    add redirects to root routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const { authenticate } = require('./middlewares/authenticate')
 
 const usersRoute = require('./api/routes/users')
 const runnersRoute = require('./api/routes/runners')
@@ -16,6 +17,14 @@ app.use(cookieParser())
 
 app.use('/user', usersRoute)
 app.use('/runner', runnersRoute)
+
+const homePageHandler = (req, res, next) => {
+  if (req.locals && req.locals.jwt) {
+    return res.redirect('http://localhost:8000/user/placeorder')
+  }
+  res.redirect('http://localhost:8000/user/getLoginURL')
+}
+app.get('/', authenticate, homePageHandler)
 
 mongoose.connect('mongodb://localhost:27017/dunzoClone', { useNewUrlParser: true })
 
