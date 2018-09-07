167b1ee3515041ba5002c4f044e26e8e89c499f8
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-09-07 13:33:34 +0530

    remove authentication from login

--- api/routes/runners.js
+++ api/routes/runners.js
@@ -3,7 +3,7 @@ const runnersController = require('../controllers/runners')
 const { authenticate } = require('../../middlewares/authenticate')
 
 router.get('/login', runnersController.getLoginUrl)
-router.get('/oauthcallback', authenticate, runnersController.oauthcallback)
+router.get('/oauthcallback', runnersController.oauthcallback)
 router.get('/profile', authenticate, runnersController.getRunnerProfile)
 router.get('/currentOrder', authenticate, runnersController.getCurrentOrder)
 router.get('/pastOrders', authenticate, runnersController.getPastOrders)
