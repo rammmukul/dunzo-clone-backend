1c913f0b62348159aee8d2f1d881112d0aca529e
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-09-07 13:35:58 +0530

    make take and fulfill order routes post

--- api/routes/runners.js
+++ api/routes/runners.js
@@ -7,7 +7,7 @@ router.get('/oauthcallback', runnersController.oauthcallback)
 router.get('/profile', authenticate, runnersController.getRunnerProfile)
 router.get('/currentOrder', authenticate, runnersController.getCurrentOrder)
 router.get('/pastOrders', authenticate, runnersController.getPastOrders)
-router.get('/takeorder', authenticate, runnersController.takeOrder)
-router.get('/fullfillorder', authenticate, runnersController.fullfillOrder)
+router.post('/takeorder', authenticate, runnersController.takeOrder)
+router.post('/fulfillorder', authenticate, runnersController.fulfillOrder)
 
 module.exports = router
