49a9df4031554ad39ecd60f80e12e1123c12ba44
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-09-12 15:19:01 +0530

    add position update

--- socketServer.js
+++ socketServer.js
@@ -1,11 +1,30 @@
 const http = require('./server')
 const io = require('socket.io')(http)
+const JWT = require('jsonwebtoken')
+const { privateKey } = require('./secrets/jwtPrivateKey')
+const haversine = require('haversine')
 
-io.on('connection', function (socket) {
-  const jwt = socket.request.headers.cookie.split(';')
-    .map(e => e.trim())
-    .filter(e => e.startsWith('access_token='))[0]
-    .substring(13)
+const runnerPos = {}
+
+io.on('connection', async function (socket) {
+  let user
+  {
+    const jwt = socket.request.headers.cookie.split(';')
+      .map(e => e.trim())
+      .filter(e => e.startsWith('access_token='))[0]
+      .substring(13)
+    user = await JWT.verify(jwt, privateKey)
+  }
+  console.log('a user connected', user)
+
+  socket.on('position update', (pos) => {
+    if (
+      !runnerPos[user] ||
+      !haversine(runnerPos[user], pos, {unit: 'meter', format: '[lon,lat]', threshold: 10})
+    ) {
+      runnerPos[user] = pos
+    }
+  })
   socket.on('disconnect', () => console.log('no more'))
 })
 
