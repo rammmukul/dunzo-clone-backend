41597158cbbccd9f5c8e11c9cb1d3a6380ac5d00
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-09-14 02:56:27 +0530

    add chat

--- socketServer.js
+++ socketServer.js
@@ -7,6 +7,7 @@ const Runners = require('./api/models/runners')
 const Orders = require('./api/models/orders')
 
 const runnerPos = {}
+const messages = {}
 
 io.on('connection', async function (socket) {
   let user
@@ -20,24 +21,57 @@ io.on('connection', async function (socket) {
     console.log('somthing wrong with jwt', e)
   }
 
-  socket.on('position update', async (pos) => {
-    if (
-      !runnerPos[user.email] ||
-      !haversine(runnerPos[user.email].pos, pos, {unit: 'meter', format: '[lon,lat]', threshold: 10})
-    ) {
-      runnerPos[user.email] = {...runnerPos[user.email], pos}
-      await Runners.update({emailID: user.email}, {location: {coordinates: pos}})
-    }
-    const sendPosTo = runnerPos[user.email].socket
-    if (sendPosTo) sendPosTo.emit('runner position', pos)
-  })
+  if (user.type === 'user') {
+    let runnerAssigned
+    socket.on('position request', async (order) => {
+      runnerAssigned = (await Orders.findOne({_id: order, status: 'assigned'}).populate('runner')).runner
+      const runnerPosition = runnerPos[runnerAssigned.emailID] ? runnerPos[runnerAssigned.emailID].pos : null
+      runnerPos[runnerAssigned.emailID] = {...runnerPos[runnerAssigned.emailID], socket}
+      socket.emit('runner position', runnerPosition)
+    })
 
-  socket.on('position request', async (order) => {
-    const runnerAssigned = (await Orders.findOne({_id: order, status: 'assigned'}).populate('runner')).runner
-    const runnerPosition = runnerPos[runnerAssigned.emailID] ? runnerPos[runnerAssigned.emailID].pos : null
-    runnerPos[runnerAssigned.emailID] = {...runnerPos[runnerAssigned.emailID], socket}
-    socket.emit('runner position', runnerPosition)
-  })
+    socket.on('join chat room', async (orderID) => {
+      runnerAssigned = (await Orders.findOne({_id: orderID, status: 'assigned'}).populate('runner')).runner
+      socket.join(runnerAssigned.email + orderID)
+      messages[runnerAssigned.email + orderID] = {
+        ...messages[runnerAssigned.email + orderID]
+      }
+      socket.emit('past messages', messages[runnerAssigned.email + orderID])
+    })
+
+    socket.on('chat message', async ([orderID, message]) => {
+      const msgObj = {from: 'runner', message}
+      socket.to(runnerAssigned.email + orderID).emit('chat message', msgObj)
+      messages[runnerAssigned.email + orderID] = msgObj
+    })
+  } else {
+    socket.on('position update', async (pos) => {
+      if (
+        !runnerPos[user.email] ||
+        !haversine(runnerPos[user.email].pos, pos, {unit: 'meter', format: '[lon,lat]', threshold: 10})
+      ) {
+        runnerPos[user.email] = {...runnerPos[user.email], pos}
+        await Runners.update({emailID: user.email}, {location: {coordinates: pos}})
+      }
+
+      const sendPosTo = runnerPos[user.email].socket
+      if (sendPosTo) sendPosTo.emit('runner position', pos)
+    })
+
+    socket.on('join chat room', async (orderID) => {
+      socket.join(user.email + orderID)
+      messages[user.email + orderID] = {
+        ...messages[user.email + orderID]
+      }
+      socket.emit('past messages', messages[user.email + orderID])
+    })
+
+    socket.on('chat message', async ([orderID, message]) => {
+      const msgObj = {from: 'user', message}
+      socket.to(user.email + orderID).emit('chat message', msgObj)
+      messages[user.email + orderID] = msgObj
+    })
+  }
 
   socket.on('disconnect', () => console.log('no more'))
 })
